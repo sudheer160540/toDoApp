7cf334362fd538560bd4c8c70e919ff3a9b18b77
sudheer160540 <sudheernunna510@gmail.com>
2018-08-14 15:03:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ console.log('asdfaa');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing your favicon in /publicsss
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
